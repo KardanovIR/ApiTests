df121e225de69385795c8e9dc942e66223346e03
peterz <peterz@rambler.ru>
2018-01-10 16:40:46 +0300

    Converted to JUnit

--- src/main/java/BurnTransactionSuite.java
+++ src/main/java/BurnTransactionSuite.java
@@ -1,16 +1,27 @@
+import com.wavesplatform.wavesj.Node;
 import com.wavesplatform.wavesj.PrivateKeyAccount;
 import com.wavesplatform.wavesj.Transaction;
+import org.junit.Test;
 
 import java.io.IOException;
+import java.net.URISyntaxException;
 
 public class BurnTransactionSuite {
+    private final Node node;
     private final static long FEE = 100_000;
 
     private final PrivateKeyAccount account = PrivateKeyAccount.fromSeed(
             "glimpse food loud wasp robust story soon mad width play grid still doctor glimpse humble", 0, 'T');
     private final String assetId = "7xf8zHJsNJ7ndyCjVWadHvWdoMc1VnfqD3FgQskTJLtT";
 
-    private Transaction sendBurnTx() throws IOException {
-        return Transaction.makeBurnTx(account, assetId, 1, FEE);
+    private BurnTransactionSuite() throws IOException, URISyntaxException {
+        node = new Node("https://testnode1.wavesnodes.com");
+    }
+
+    @Test
+    private void test() throws IOException {
+        Transaction burnTx = Transaction.makeBurnTx(account, assetId, 1, FEE);
+        String burnTxId = node.send(burnTx);
+        System.out.println("Burned with tx id " + burnTxId);
     }
 }
